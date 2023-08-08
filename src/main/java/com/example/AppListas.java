f50bf1bd1174f5b337a644ef2d853c386c6a9daa
Augusto Freitas <cesarfreitas.rn@gmail.com>
2023-08-08 21:21:31 +0000

    Adicionado AppListas

---
+++ src/main/java/com/example/AppListas.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import java.util.ArrayList;
+
+public class AppListas {
+    List<Clientes> listaClientes = new ArrayList<>();
+
+    Cliente Cliente1 = new Cliente();
+    Cliente.setNome("Marcio");
+    
+}
